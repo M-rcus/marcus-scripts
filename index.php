602d3c18868dc7053e99e05a3ef687cd5ae405e8
Marcus <marcus@marcus.pw>
2019-08-12 01:10:56 +0200

    Add index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marcus - Scripts</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.extend.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="card border-primary top-margin">
+            <h2 class="card-header bg-primary text-white">Scripts:</h2>
+            <div class="card-body">
+                Welcome to my page dedicated for providing basic scripts to do stuff.
+                <div class="list-group">
+                    <a href="./ffmpeg-volume-mute" class="list-group-item">ffmpeg command generator for muting audio at timestamps.</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
