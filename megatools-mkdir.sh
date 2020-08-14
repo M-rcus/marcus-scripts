17afa792e68e475a4ca4ef8fa2c2a5cf6e7843a6
Marcus <marcus@marcus.pw>
2020-08-14 18:21:09 +0200

    [Mega] Add helper script for megatools

---
+++ megatools-mkdir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FOLDER_NAME="$@";
+
+if [[ -z "${FOLDER_NAME}" ]]; then
+    echo "Please specify folder name.";
+    exit 0
+fi
+
+megatools mkdir "/Root/${FOLDER_NAME}";
+
+echo "Run copy command? [y/N]"
+read RUN_COPY
+
+RUN_COPY=${RUN_COPY,,}
+if [[ $RUN_COPY == *"y"* ]]; then
+    megatools copy --remote "/Root/${FOLDER_NAME}" --local "${FOLDER_NAME}";
+else
+    exit 0
+fi
