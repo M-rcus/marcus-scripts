b15230d69316a14f341c41ca805045304c213ebb
Marcus <marcus@marcus.pw>
2020-10-19 18:52:18 +0200

    [OnlyFans] Add script for sorting creator list in clipboard

---
+++ onlyfans-sort-creator-clipboard-list.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+/**
+ * Sort clipboard copy-paste from DIGITALCRIMINAL/OnlyFans into a unique, alphabetical list.
+ * Useful when using multiple auths and a lot of shared creators (especially free ones...).
+ */
+
+const clipboardy = require('clipboardy');
+const reg = / ?\| \d+ = /g;
+const input = clipboardy.readSync();
+
+if (!input) {
+    console.error('[OnlyFans Sort Clipboard] Nothing in clipboard');
+    process.exit(1);
+}
+
+const list = input
+             // Replace the beginning part
+             .replace('Names: Username = username | 0 = All ')
+             // 'Split' the string into a newline-based list
+             .replace(reg, '\n')
+             // Trim the list (remove leading/trailing whitespace)
+             .trim();
+
+const unique = list
+               // Split the list into an array
+               .split('\n')
+               // Sort it alphabetically, ascending
+               .sort()
+               // Filter unique values
+               .filter((value, index, self) => {
+                   return self.indexOf(value) === index;
+               });
+
+// Join back the filtered list into a newline-based list
+const text = unique.join('\n');
+// Write the filtered list to clipboard
+clipboardy.writeSync(text);
+console.log('[OnlyFans Sort Clipboard] Sorted/filtered list written to clipboard.');
